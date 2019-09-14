29d24f2d2132354c98a45227b11dfa7a194b335c
tang485 <54291675+tang485@users.noreply.github.com>
2019-09-14 19:22:18 -0400

    Basic Combat Functions

--- Assets/C#/Player.cs
+++ Assets/C#/Player.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    int attack;
+    int health;
+    float lastAttack = 0f;
+    float attackSpeed = 0f;
+    float range;
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 100;
+        attack = 1;
+        attackSpeed = 1;
+        range = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public float calculateDistance(Player there)
+    {
+        return Vector3.Distance(transform.position, there.position);
+    }
+
+    public void attack(Player target)
+    {
+        if (Time.time >= lastAttack + 1/attackSpeed && calculateDistance(target) <= range) { 
+            target.takeDamage(attack);
+            lastAttack = Time.time;
+        }
+    }
+
+    public void takeDamage(int damage)
+    {
+        health -= damage;  
     }
 }

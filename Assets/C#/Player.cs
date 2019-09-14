66e099554c3c712960271c2e424ad97f00d17b5a
Riley Borgard <rborgard@purdue.edu>
2019-09-14 18:51:35 -0400

    basic player movement with arrow keys

--- Assets/C#/Player.cs
+++ Assets/C#/Player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D body;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float dx = Input.GetAxis("Horizontal");
+        float dy = Input.GetAxis("Vertical");
+        body.velocity = new Vector2(dx, dy) * speed;
     }
 }

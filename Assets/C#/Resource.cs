d19952d4c176d11faceacc6cf671363b1691d913
TheNerdv2 <mbeshear@purdue.edu>
2019-10-01 19:10:25 -0400

    Added resource definition
    Added a resource class with defined values for different types of resources

---
+++ Assets/C#/Resource.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+    public readonly int WOOD = 1;
+    public readonly int STONE = 2;
+    public readonly int COPPER = 3;
+    public readonly int IRON = 4;
+    public readonly int GOLD = 5;
+    public readonly int DIAMOND = 6;
+
+    protected int type;
+    protected int amount;
+
+    public Resource()
+    {
+        type = 0;
+        amount = 0;
+    }
+
+    public Resource(int t, int a)
+    {
+        type = t;
+        amount = a;
+    }
+
+    public int getType()
+    {
+        return type;
+    }
+
+    public int getAmount()
+    {
+        return amount;
+    }
+
+    public void setAmount(int a)
+    {
+        amount = a;
+    }
+}

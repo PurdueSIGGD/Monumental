6583b292beeb992d80de1640473b96b0f66e277c
TheNerdv2 <mbeshear@purdue.edu>
2019-10-15 19:37:33 -0400

    Added intial declaration of Upgrade class

--- Assets/C#/Upgrades/Upgrade.cs
+++ Assets/C#/Upgrades/Upgrade.cs
@@ -2,21 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Type
+public enum UpgradeType
 {
-    Health = 1,
-    Movement = 1,
-    Interaction = 1,
-    Gather = 2,
-    Melee = 2,
-    Range = 2,
-    Monument = 3
+    Health = 0,
+    Movement = 0,
+    Interaction = 0,
+    Gather = 1,
+    Melee = 1,
+    Range = 1,
+    Monument = 2
 }
 
-public struct Upgrade
+enum UpgradeAmount
 {
-    Type type;
-    int tier;
+    small = 10,
+    large = 100
+}
 
-    
+public class Upgrade
+{
+    public UpgradeType type;
+    public int tier;
+    public SyncListResource cost = new SyncListResource();
+
+    public Upgrade(UpgradeType t, int r)
+    {
+        type = t;
+        tier = r;
+
+        Resource small = new Resource();
+        small.type = (ResourceName)(2*r - (int)type); //Utilizes lookup function - Ask Michael Beshear if confused
+        small.amount = (int)UpgradeAmount.small;
+
+        cost.Add(small);
+
+        Resource large = new Resource();
+        large.type = (ResourceName)(2*r + (int)type - 1);
+        large.amount = (int)UpgradeAmount.large;
+
+        cost.Add(large);
+    }
 }

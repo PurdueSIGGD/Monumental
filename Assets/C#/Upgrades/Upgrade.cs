ab6c9ea1a62676dfe7855d2a0c707f54c67b0800
TheNerdv2 <mbeshear@purdue.edu>
2019-10-15 18:48:32 -0400

    Inital Upgrade definition

---
+++ Assets/C#/Upgrades/Upgrade.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Type
+{
+    Health = 1,
+    Movement = 1,
+    Interaction = 1,
+    Gather = 2,
+    Melee = 2,
+    Range = 2,
+    Monument = 3
+}
+
+public struct Upgrade
+{
+    Type type;
+    int tier;
+
+    
+}

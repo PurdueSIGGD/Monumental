32e2950dc908beec49e8be71395b8e9770b0fb95
TheNerdv2 <mbeshear@purdue.edu>
2019-10-19 18:45:43 -0400

    Initial upgrade implimentation complete
    Fully implimented the upgrade class.
    Implimented UpdateStatsAndCost funcion that a base calls when purchasing an upgrade.
    Added enums and static varibles that can be easily changed for game balance

--- Assets/C#/Upgrades/Upgrade.cs
+++ Assets/C#/Upgrades/Upgrade.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//What each type of upgrade affects in player stats
 public enum UpgradeType
 {
     Health = 0,
@@ -13,7 +14,9 @@ public enum UpgradeType
     Monument = 2
 }
 
-enum UpgradeAmount
+//Base cost of an upgrade of the lesser and greater resource costs
+//Can be updated for balance
+enum UpgradeCost
 {
     small = 10,
     large = 100
@@ -25,6 +28,15 @@ public class Upgrade
     public int tier;
     public SyncListResource cost = new SyncListResource();
 
+    //Value sliders that can be chaged for balance
+    static int Health = 30;
+    static float Movement = 0.25f;
+    static float Interaction = 0.1f;
+    static float Gather = 0.1f;
+    static int Melee = 20;
+    static int Ranged = 10;
+    static float costScale = 1.2f;
+
     public Upgrade(UpgradeType t, int r)
     {
         type = t;
@@ -32,14 +44,36 @@ public class Upgrade
 
         Resource small = new Resource();
         small.type = (ResourceName)(2*r - (int)type); //Utilizes lookup function - Ask Michael Beshear if confused
-        small.amount = (int)UpgradeAmount.small;
+        small.amount = (int)UpgradeCost.small;
 
         cost.Add(small);
 
         Resource large = new Resource();
         large.type = (ResourceName)(2*r + (int)type - 1);
-        large.amount = (int)UpgradeAmount.large;
+        large.amount = (int)UpgradeCost.large;
 
         cost.Add(large);
+    }
+
+    //Function that base calls to purchase an upgrade
+    public void UpdateStatsAndCost(PlayerStats ps)
+    {
+        if(type == UpgradeType.Health)
+        {
+            ps.health += Health;
+            ps.movementSpeed += Movement;
+            ps.interactionSpeed += Interaction;
+        }
+        else if(type == UpgradeType.Gather)
+        {
+            ps.gatherAmount += Gather;
+            ps.meleeDamage += Melee;
+            ps.rangedDamage += Ranged;
+        }
+
+        foreach(Resource res in cost)
+        {
+            res.setAmount((int)(res.amount * costScale));
+        }
     }
 }

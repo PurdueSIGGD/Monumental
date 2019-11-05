82d1c046f60f387964ddd847e8c2a9a7018c195e
andyw24 <andyw24@gmail.com>
2019-11-05 18:28:55 -0500

    fixing merge conflicts

--- Assets/C#/PlayerScripts/ShootingProjectiles.cs
+++ Assets/C#/PlayerScripts/ShootingProjectiles.cs
@@ -7,8 +7,8 @@ public class ShootingProjectiles : MonoBehaviour
 	private bool canShoot = true; //is true if player's hitbox isn't hitting a resource or enemy player
 	private Rigidbody2D projectilePrefab;
 	private PlayerStats stats;
-	public float yValue = 1f; // Used to make it look like it's shot from the gun itself (offset)
-	public float xValue = 0.2f; // Same as above
+	public float yValue; // Used to make it look like it's shot from the gun itself (offset)
+	public float xValue; // Same as above
 
 	public void cannotShoot()
 	{
@@ -30,9 +30,10 @@ public class ShootingProjectiles : MonoBehaviour
     {
 		if (Input.GetMouseButtonDown(0) && canShoot)
 		{
+			Transform hitbox = transform.Find("Hitbox Pivot Point");
 			Rigidbody2D newProjectile = Instantiate(projectilePrefab, new Vector3(transform.position.x + xValue, transform.position.y + yValue, transform.position.z), Quaternion.identity) as Rigidbody2D;
 			Projectile newProjectileProperties = newProjectile.gameObject.GetComponent<Projectile>();
-			newProjectile.AddForce(transform.right * stats.projectileSpeed);
+			newProjectile.AddForce(hitbox.right * stats.projectileSpeed);
 			newProjectileProperties.parentGameobject = gameObject;
 			newProjectileProperties.damage = stats.rangedDamage;
 			newProjectileProperties.teamIndex = gameObject.GetComponent<Player>().teamIndex;
